fb9fd2383dd0dbc7f5e354b262ed57d567506447
Michał Świętek <mswietku@gmail.com>
2017-12-27 11:06:22 +0100

    Add DragWidget to the interface

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "dragwidget.h"
-#include "QHBoxLayout"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QWidget w;
+    MainWindow m;
 
-    QHBoxLayout *horizontalLayout = new QHBoxLayout(&w);
-    QTabWidget *tabWidget = new QTabWidget();
-
-    tabWidget->addTab(new DragWidget(false), "First tab");
-
-    horizontalLayout->addWidget(tabWidget);
-    horizontalLayout->addWidget(new DragWidget(true, 900, 500));
-
-    w.show();
-
+    m.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "dragwidget.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
+    ui->tabWidget->addTab(new DragWidget(false), "First tab");
 }
 
 MainWindow::~MainWindow()
